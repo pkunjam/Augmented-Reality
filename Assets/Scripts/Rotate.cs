e1a3ac2d1569aed18a3220db60c33598e85a4a51
pkunjam <punit17100@iiitnr.edu.in>
2020-05-07 01:57:37 +0530

    cube rotation

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    public int speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    transform.Rotate(new Vector3(0,Time.deltaTime*speed,0));
+	}
+}
