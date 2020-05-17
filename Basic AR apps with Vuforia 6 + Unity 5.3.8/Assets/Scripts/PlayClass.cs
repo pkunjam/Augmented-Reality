c3189c227444d1aa67fbd7e8ddf93d66ccf88a81
pkunjam <punit17100@iiitnr.edu.in>
2020-05-17 16:33:47 +0530

    final

---
+++ Basic AR apps with Vuforia 6 + Unity 5.3.8/Assets/Scripts/PlayClass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayClass : MonoBehaviour {
+
+    public MovieTexture movTexture;
+	void Start () {
+        GetComponent<Renderer>().material.mainTexture = movTexture;
+        movTexture.Play();
+        movTexture.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Basic AR apps with Vuforia 6 + Unity 5.3.8/Assets/Scripts/VBscript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+public class VBscript : MonoBehaviour, IVirtualButtonEventHandler {
+
+    GameObject VButtonObject;
+	// Use this for initialization
+	void Start () {
+        VButtonObject = GameObject.Find("RotateButton");
+        VButtonObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler (this);
+	}
+
+    public void OnButtonPressed(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("button pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonAbstractBehaviour vb)
+    {
+        Debug.Log("button released");
+    }
+}
